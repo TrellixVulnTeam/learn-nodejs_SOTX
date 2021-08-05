19feb4c94b19658742d7b958a0270b62e11f9b5f
NHH <nghuyhoang2509@gmail.com>
2021-08-05 09:54:35 +0700

    nodejs test index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const express= require('express')
+const app= express()
+const port= 3000;
+
+app.get('/home',(req,res) => res.send('Hello'))
+
+app.listen(port,() => console.log(`${port} thành công`))
